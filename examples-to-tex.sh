d73954609e0865a690cc269bf9a7475431a97951
Yegor Bugayenko <yegor256@gmail.com>
2025-11-12 15:33:31 +0300

    Refactor examples-to-tex.sh script for better file handling.

--- examples-to-tex.sh
+++ examples-to-tex.sh
@@ -7,5 +7,7 @@ set -e -o pipefail
 dir=$1
 
 while IFS= read -r f; do
-    phino merge "${f}" runtime.phi | phino rewrite --normalize --nonumber --sequence --output=latex --flat --sweet
+    e=$(basename "${f}")
+    e=${e//.phi}
+    phino merge "${f}" runtime.phi | phino rewrite --normalize --hide=Q.org "--expression=${e}" --nonumber --sequence --output=latex --flat --sweet
 done < <(find "${dir}" -name '*.phi' -type f)
