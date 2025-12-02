b4e28f79db30d058e7772b37b16fbadf34f2e573
Yegor Bugayenko <yegor256@gmail.com>
2025-12-02 09:53:24 +0300

    Refactor cell creation logic into JavaScript.

---
+++ cells/application.js
@@ -0,0 +1,15 @@
+const application = (prototype, attribute, argument, memo) => ({
+  take(attr) { return this.m().take(attr); },
+  put() { throw new Error('put is not allowed'); },
+  delta() { return this.m().delta(); },
+  copy() { return this; },
+  memo: null,
+  m() {
+    if (memo === null) {
+      const c = prototype.copy();
+      c.put(attribute, argument);
+      memo = c;
+    }
+    return memo;
+  }
+});
