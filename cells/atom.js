40d315c3c52e2d551daee4334f1a0874c191ec07
Yegor Bugayenko <yegor256@gmail.com>
2025-12-02 10:46:25 +0300

    Fix missing method call in atom.js.

--- cells/atom.js
+++ cells/atom.js
@@ -1,7 +1,7 @@
 atom = (lambda, base) => ({
   take(attr) {
     try { return base.take(attr); }
-    catch { return lambda(this); }
+    catch { return lambda(this).take(attr); }
   },
   put(attr, cell) { base.put(attr, cell); },
   delta() { return base.delta(); },
