90b1a8f069328a7a8a1ffa2b7a867736501d32fd
Yegor Bugayenko <yegor256@gmail.com>
2025-12-18 10:23:34 +0300

    Remove unnecessary files and directories.

--- zip-it.sh
+++ zip-it.sh
@@ -40,8 +40,10 @@ pdflatex -shell-escape -halt-on-error paper.tex
 bibtex paper
 pdflatex -halt-on-error paper.tex
 pdflatex -halt-on-error paper.tex
-rm -rf ./*.aux ./*.bcf ./*.blg ./*.fdb_latexmk ./*.fls ./*.log ./*.run.xml ./*.out ./*.exc ./*.ret
+rm -rf ./*.aux ./*.bcf ./*.blg ./*.fdb_latexmk ./*.fls ./*.log ./*.run.xml ./*.out ./*.exc ./*.ret ./*.sh
 rm -rf bibliography
+rm -rf examples
+rm -rf _eolang/*.pl
 zip="paper-${version}.zip"
 zip -x paper.pdf -r "${zip}" ./*
 echo "🍒 ZIP is ready for arXiv at ${zip} ($(du -sh "${zip}" | cut -f1))"
